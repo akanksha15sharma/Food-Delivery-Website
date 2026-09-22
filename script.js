bde28513db495c72cfdbea8ecb3206fcb8df9b53
Akanksha <akankshasharma15082005@gmail.com>
2026-09-22 22:28:30 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
- var swiper = new Swiper('.mySwiper', {
-    loop: true,
-        navigation: {
-          nextEl: '#next',
-          prevEl: '#prev',
-        },
-      });
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
+});
 
-const cartIcon = document.querySelector('.cart-icon');
-const cartTab = document.querySelector('.cart-tab');
-const closeBtn = document.querySelector('.close-btn')
+const cartIcon = document.querySelector(".cart-icon");
+const cartTab = document.querySelector(".cart-tab");
+const closeBtn = document.querySelector(".close-btn");
+const cardLlist = document.querySelector('.card-list')
 
-cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
-closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
+cartIcon.addEventListener("click", () =>
+  cartTab.classList.add("cart-tab-active"),
+);
+closeBtn.addEventListener("click", () =>
+  cartTab.classList.remove("cart-tab-active"),
+);
 
 let productList = [];
 
-const initApp = ()=> {
-    fetch('products.json').then(response => response.json().then
-(data => {
-    productList = data;
-}))
-}
+const showCards = () => {
+  productList.forEach((product) => {
+    const orderCard = document.createElement("div");
+    orderCard.classList.add("order-card");
+
+    orderCard.innerHTML = `
+          <div class="card-image">
+             <img src="${product.image}" alt="img" />
+          </div>
+          <h4>${product.name}</h4>
+          <h4 class="price">${product.price}</h4>
+          <a href="#" class="btn">Add to Cart</a>`;
+
+    cardLlist.appendChild(orderCard);
+
+
+
+  });
+};
+
+const initApp = () => {
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      productList = data;
+      showCards();
+    });
+};
 
 initApp();
